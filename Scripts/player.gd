2e6bfe8973fa8791af8dd41b6b9c1083188a3753
Jorge Virgos <164354042+jorge-kabuto@users.noreply.github.com>
2025-01-25 15:46:56 +0100

    player anims not good. need queue

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,25 @@
 extends Node3D
 
+var player_sprite = AnimatedSprite3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	#player_sprite = get_node("AnimatedSprite3D")
+	$AnimatedSprite3D.play("idle")
+	
+	$AnimatedSprite3D.animation_finished.connect(_animation_finished)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _animation_finished():
+	$AnimatedSprite3D.play("idle")
+	
+func eat_anim():
+	$AnimatedSprite3D.play("eat")
+func attack_anim():
+	$AnimatedSprite3D.play("attack")
+func react_to_damage_anim():
+	$AnimatedSprite3D.play("react_to_damage")
+func spawn_anim():
+	$AnimatedSprite3D.play("spawn")
