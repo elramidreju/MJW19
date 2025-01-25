02adc6b9e385577aaab307fec920202bc342c39a
mbmdevelop <54529683+mbmdevelop@users.noreply.github.com>
2025-01-25 12:36:38 +0100

    UIDie de verdad

--- Scripts/ui_die.gd
+++ Scripts/ui_die.gd
@@ -3,8 +3,19 @@ class_name UI_Die
 extends Control
 
 @export var faces:int = 0
-var die_scene = preload("res://scenes/ui_die.tscn")
+#var die_scene = preload("res://scenes/ui_die.tscn")
+signal on_playerdiceroll(roll_die_result)
+
+func _update_ui() -> void:
+	$Label.text = "D" + str(faces)
+
+func _ready() -> void:
+	_update_ui()
+
+func _roll_die() -> void:
+		var roll_die_result = randi_range(1, faces)
+		on_playerdiceroll.emit(roll_die_result)
 
 func _on_texture_button_pressed() -> void:
-	var test:int = 0
+	_roll_die()
 	pass # Replace with function body.
